df7cfde48e56e6c493e98c2057942588f302081e
Francois Perron <fperron@gmail.com>
2017-12-01 11:14:05 -0500

    Day 1 part 2

--- src/day01/captcha.js
+++ src/day01/captcha.js
@@ -1,8 +1,20 @@
+const toNumbers = (digits) => digits.split('').map(d => parseInt(d))
+
+const matches = (n1, n2) => n1 === n2
+
 const captcha = (digits) => {
-  const numbers = digits.split('').map(d => parseInt(d))
+  const numbers = toNumbers(digits)
   numbers.push(numbers[0])
 
-  return numbers.reduce((sum, n, i, numbers) => n === numbers[i + 1] ? sum + n : sum, 0)
+  return numbers.reduce((sum, n, i, numbers) => matches(n, numbers[i + 1]) ? sum + n : sum, 0)
 }
 
-export { captcha }
+const captcha2 = (digits) => {
+  const numbers = [...toNumbers(digits), ...toNumbers(digits)]
+
+  const digitsSize = digits.length
+  const step = digitsSize / 2
+  return numbers.reduce((sum, n, i, numbers) => matches(n, numbers[i + step]) && i < digitsSize ? sum + n : sum, 0)
+}
+
+export { captcha, captcha2 }
