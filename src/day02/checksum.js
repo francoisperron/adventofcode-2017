5588376afb4a2156fab9779e0d38b0bf608b3d7e
Francois Perron <fperron@gmail.com>
2017-12-02 16:01:19 -0500

    Day 2 part 2 - convert double for loops with reduce and filter

--- src/day02/checksum.js
+++ src/day02/checksum.js
@@ -1,18 +1,17 @@
 const minMaxDiff = (line) => {
   line.sort((a, b) => a - b)
-  return line[line.length-1] - line[0]
+  return line[line.length - 1] - line[0]
 }
 
+const isEvenlyDivisible = (dividend, divisor) => dividend !== divisor && dividend % divisor === 0
+
 const divisible = (line) => {
-  for (let i = 0; i < line.length; i++) {
-    let n = line[i]
-    for (let j = 0; j < line.length; j++) {
-      let divider = line[j]
-      if (i !== j && n % divider === 0){
-        return n / divider
-      }
-    }
-  }
+  const {dividend, divisor} = line.reduce((found, dividend) => {
+    const divisors = line.filter(divisor => isEvenlyDivisible(dividend, divisor))
+    return divisors.length > 0 ? {dividend: dividend, divisor: divisors[0]} : found
+  }, {})
+
+  return dividend / divisor
 }
 
 const checksum = (spreadsheet, algo) => spreadsheet.reduce((sum, line) => sum + algo(line), 0)
