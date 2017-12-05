356734880d0fbfd4c2308e988b35f247481388da
Francois Perron <fperron@gmail.com>
2017-12-05 08:59:32 -0500

    Day 5 extract jump methods

--- src/day05/cpu.js
+++ src/day05/cpu.js
@@ -1,26 +1,25 @@
-export const jumpsUntilOutside = (instructions) => {
+const hopOne = () => 1
+
+const strangeJump = (offset) => offset >= 3 ? -1 : 1
+
+export const jumps = (instructions, jumpMethod) => {
   let index = 0
   let steps = 0
 
   while (index < instructions.length) {
     let lastIndex = index
-    index = index + instructions[index]
-    instructions[lastIndex]++
-    steps++
-  }
-  return steps
-}
-
-export const strangeJumpsUntilOutside = (instructions) => {
-  let index = 0
-  let steps = 0
-
-  while (index < instructions.length) {
-    let lastIndex = index
-    let jump = instructions[index] >= 3 ? -1 : 1
+    let jump = jumpMethod(instructions[index])
     index = index + instructions[index]
     instructions[lastIndex] += jump
     steps++
   }
   return steps
+}
+
+export const jumpsUntilOutside = (instructions) => {
+  return jumps(instructions, hopOne)
+}
+
+export const strangeJumpsUntilOutside = (instructions) => {
+  return jumps(instructions, strangeJump)
 }
